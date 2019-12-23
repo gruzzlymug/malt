4bb90fdff8d89f7943db22b9db1a2b2e2089702a
Kevin Guran <kevin@arturo.ai>
2019-12-23 14:38:50 -0600

    Add message processing

--- 2/app.py
+++ 2/app.py
@@ -33,6 +33,60 @@ def maybe_download(url):
     return unzipped
 
 
+def clean_message_types(df):
+    df.columns = [c.lower().strip() for c in df.columns]
+    df.value = df.value.str.strip()
+    df.name = (df.name
+               .str.strip()
+               .str.lower()
+               .str.replace(' ', '_')
+               .str.replace('-', '_')
+               .str.replace('/', '_'))
+    df.notes = df.notes.str.strip()
+    df['message_type'] = df.loc[df.name == 'message_type', 'value']
+    return df
+
+
+def format_alpha(mtype, data):
+    """Process byte strings of type alpha"""
+    for col in alpha_formats.get(mtype).keys():
+        if mtype != 'R' and col == 'stock':
+            data = data.drop(col, axis=1)
+            continue
+        data.loc[:, col] = data.loc[:, col].str.decode("utf-8").str.strip()
+        if encoding.get(col):
+            data.loc[:, col] = data.loc[:, col].map(encoding.get(col))
+    return data
+
+
+def store_messages(m):
+    """Handle occasional storing of all messages"""
+    with pd.HDFStore(itch_store) as store:
+        for mtype, data in m.items():
+            # convert to DataFrame
+            data = pd.DataFrame(data)
+
+            # parse timestamp info
+            data.timestamp = data.timestamp.apply(int.from_bytes, byteorder='big')
+            data.timestamp = pd.to_timedelta(data.timestamp)
+
+            # apply alpha formatting
+            if mtype in alpha_formats.keys():
+                data = format_alpha(mtype, data)
+
+            s = alpha_length.get(mtype)
+            if s:
+                s = {c: s.get(c) for c in data.columns}
+            dc = ['stock_locate']
+            if m == 'R':
+                dc.append('stock')
+            store.append(mtype,
+                         data,
+                         format='t',
+                         min_itemsize=s,
+                         data_columns=dc)
+
+
 data_path = Path('data')
 itch_store = str(data_path / "itch.h5")
 order_book_store = data_path / "order_book.h5"
@@ -72,5 +126,76 @@ formats = {
 message_data = (pd.read_excel('message_types.xlsx', sheet_name='messages', encoding='latin1')
                 .sort_values('id')
                 .drop('id', axis=1))
+message_types = clean_message_types(message_data)
+message_labels = (message_types.loc[:, ['message_type', 'notes']]
+                  .dropna()
+                  .rename(columns={'notes': 'name'}))
+message_labels.name = (message_labels.name
+                       .str.lower()
+                       .str.replace('message', '')
+                       .str.replace('.', '')
+                       .str.strip().str.replace(' ', '_'))
+# message_labels.to_csv('message_labels.csv', index=False)
+# message_types = pd.read_csv('message_types.csv')
+print(message_labels.head())
+
+message_types.loc[:, 'formats'] = (message_types[['value', 'length']]
+                                   .apply(tuple, axis=1).map(formats))
+
+alpha_fields = message_types[message_types.value == 'alpha'].set_index('name')
+alpha_msgs = alpha_fields.groupby('message_type')
+alpha_formats = {k: v.to_dict() for k, v in alpha_msgs.formats}
+alpha_length = {k: v.add(5).to_dict() for k, v in alpha_msgs.length}
+
+message_fields, fstring = {}, {}
+for t, message in message_types.groupby('message_type'):
+    message_fields[t] = namedtuple(typename=t, field_names=message.name.tolist())
+    fstring[t] = '>' + ''.join(message.formats.tolist())
+
+messages = {}
+message_count = 0
+message_type_counter = Counter()
+
+start = time()
+with filename.open('rb') as data:
+    while True:
+        # determine message size in bytes
+        message_size = int.from_bytes(data.read(2), byteorder='big', signed=False)
+
+        # get message type by reading first byte
+        message_type = data.read(1).decode('ascii')
+
+        # create data structure to capture result
+        if not messages.get(message_type):
+            messages[message_type] = []
+
+        message_type_counter.update([message_type])
+
+        # read and store message
+        record = data.read(message_size - 1)
+        message = message_fields[message_type]._make(unpack(fstring[message_type], record))
+        messages[message_type].append(message)
+
+        # deal with system events
+        if message_type == 'S':
+            timestamp = int.from_bytes(message.timestamp, byteorder='big')
+            print('\n', event_codes.get(message.event_code.decode('ascii'), 'Error'))
+            print('\t{0}\t{1:,.0f}'.format(timedelta(seconds=timestamp * 1e-9),
+                                                     message_count))
+            if message.event_code.decode('ascii') == 'C':
+                store_messages(messages)
+                break
+
+        message_count += 1
+        if message_count % 2.5e7 == 0:
+            timestamp = int.from_bytes(message.timestamp, byteorder='big')
+            print('\t{0}\t{1:,.0f}\t{2}'.format(timedelta(seconds=timestamp * 1e-9),
+                                                message_count,
+                                                timedelta(seconds=time() - start)))
+            store_messages(messages)
+            messages = {}
+
+print(timedelta(seconds=time() - start))
+
 
 print("yuck")
